7f04849a7b1c618f23d8ec2d0f93cfb5cfcf38a7
Sue Mu <mushuyun@yahoo.com>
2022-08-04 21:50:12 -0400

    connect to atlas

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(
 
 const secret = process.env.SECRET || 'shouldbeconfidential';
 const store = MongoStore.create({
-    mongoUrl: MONGO_URI,
+    mongoUrl: process.env.MONGO_URI,
     touchAfter: 24 * 60 * 60,
     crypto: {
         secret
