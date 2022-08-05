db85cceff56f12c41acf3902a903f267d1d36548
Sue Mu <mushuyun@yahoo.com>
2022-08-04 21:04:01 -0400

    security true

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const sessionConfig ={
     saveUninitialized: true,
     cookie: { 
         httpOnly: true,
-        // secure: true; //only use when deploy
+        secure: true,//only use when deploy
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge:1000 * 60 * 60 * 24 * 7
     }
